b0d8fece57a921bec90be060f16b5a085747b45d
Jansen <jmcenteeiv@gmail.com>
2023-02-04 11:06:21 -0700

    sort of get board rendering working

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+
+    public GameObject block;
+    public int width = 10;
+    public int height = 4;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                Instantiate(block, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
