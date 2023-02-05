3afd3844b29d12c0ac2f2ead82511a2695c0c3d5
rrodrig6 <rrodrig6@uccs.edu>
2023-02-04 17:30:55 -0700

    Stub out HexTile and Grass components

---
+++ Assets/Scripts/HexTile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour
+{
+
+    public GameObject Billboard; // "3D" representation of plant
+    public GameObject Tile; // Hexagonal base tile
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateVisuals()
+    {
+
+    }
+}
